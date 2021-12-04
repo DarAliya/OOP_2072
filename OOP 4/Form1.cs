442b2ab51e3d47d7db457315d3388c95e48ec218
DarAliya <81409390+DarAliya@users.noreply.github.com>
2021-12-04 09:52:50 +0300

    load

---
+++ OOP 4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MyDate mydate;
+        MyCelebration mycelebration;
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            //mydate2 = new MyDate(date_text,month_text,year_text);      
+            //mydate = new MyDate(Convert.ToInt32(date_text.Text), Convert.ToInt32(month_text.Text), Convert.ToInt32(year_text.Text));
+            bool flag = true;
+            try
+            {
+                int day1;
+                int month1;
+                int year1;
+                day1 = Convert.ToInt32(date_text.Text);
+                month1 = Convert.ToInt32(month_text.Text);
+                year1 = Convert.ToInt32(year_text.Text);
+                
+            }
+            catch
+            {                
+                proverka_text.Text = "Неверный формат даты";
+                flag = false;
+            }
+            if(flag==true)
+            {
+                mydate = new MyDate(Convert.ToInt32(date_text.Text), Convert.ToInt32(month_text.Text), Convert.ToInt32(year_text.Text), proverka_text.Text);
+                mydate.visokos();
+                mydate.IsValid();
+                if (proverka_text.Text == "")
+                {
+                    mydate.NextDate(); 
+                }
+                mydate.Visual(date_text, month_text, year_text, proverka_text);
+                mycelebration = new MyCelebration(Convert.ToInt32(date_text.Text), Convert.ToInt32(month_text.Text), Convert.ToInt32(year_text.Text));
+                mycelebration.lets_celebrate(prazdnik_text);
+            }
+            
+                     
+        }
+
+        
+    }
+}
---
+++ OOP 4/MyDate.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace OOP_4
+{
+    class MyDate
+    {
+        protected int date;
+        protected int month;
+        protected int year;
+        protected int[] days;
+        protected string proverka;
+        public int DATE
+        {
+            get { return date; }
+            set { date = value; }
+
+        }
+        public int MONTH
+        {
+            get { return month; }
+            set { month = value; }
+             
+        }
+        public int YEAR
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public string Proverka
+        {
+            get { return proverka; }
+            set { proverka = value; }
+        }
+        public MyDate(int date, int month, int year, string proverka)
+        {
+            this.date = date;
+            this.month = month;
+            this.year = year;
+            this.days = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            this.proverka = proverka;
+        }
+        public void NextDate()
+        {
+            
+            if((DATE == days[MONTH - 1])&&(MONTH==12))
+            {
+                MONTH=1;
+                DATE = 1;
+                YEAR++;
+            }
+            else if(DATE == days[MONTH - 1])
+            {
+                MONTH++;
+                DATE = 1;
+            }
+            else { DATE++; }
+        }
+        public void visokos()
+        {
+            if(YEAR%4==0)
+            {
+                this.days[1] = 29;
+            }
+            else { this.days[1] = 28; }
+        }
+        public void Visual(TextBox date, TextBox month, TextBox year, Label proverka)
+        {
+            date.Text = DATE.ToString();
+            month.Text = MONTH.ToString();
+            year.Text = YEAR.ToString();
+            proverka.Text = Proverka;
+        }
+        public void IsValid()
+        {
+            if ((MONTH == 2) && (((YEAR % 4 == 0) && (DATE > 29)) || ((YEAR % 4 != 0) && (DATE > 28))))
+            {
+                Proverka = "Неверный формат даты";
+                DATE = 22;
+                MONTH = 11;
+                YEAR = 2021;
+            }
+            else if ((DATE <= 0) || (DATE > 31) || (MONTH <= 0) || (MONTH > 12) || (YEAR <= 0))
+            {
+                Proverka = "Неверный формат даты";
+                DATE =22;
+                MONTH = 11;
+                YEAR = 2021;
+            }
+            else if (DATE > days[MONTH - 1])
+            {
+                Proverka = "Неверный формат даты";
+                DATE =22;
+                MONTH = 11;
+                YEAR = 2021;
+            }
+            else { Proverka = ""; }
+            
+        }
+    }
+}
